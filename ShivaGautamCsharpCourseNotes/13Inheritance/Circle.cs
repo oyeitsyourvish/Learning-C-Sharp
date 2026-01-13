f8f1ca03bda605fd7358b383bc99b4b71969eb68
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-01-13 06:51:45 +0530

    Writing an example of Inheritance

---
+++ ShivaGautamCsharpCourseNotes/13Inheritance/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShivaGautamCsharpCourseNotes._13Inheritance
+{
+    internal class Circle
+    {
+        internal int param1;
+        internal float area;
+
+        internal void Accept(int param1)
+        {
+            this.param1 = param1;
+        }
+
+        internal void CircleArea()
+        {
+            area = 3.14F * param1 * param1;
+        }
+
+        internal void Display()
+        {
+            Console.WriteLine("Area is {0}", area);
+        }
+    }
+}
---
+++ ShivaGautamCsharpCourseNotes/13Inheritance/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShivaGautamCsharpCourseNotes._13Inheritance
+{
+    internal class Rectangle : Circle
+    {
+        internal int param2;
+        
+        internal void Accept(int param2)
+        {
+            this.param2 = param2;
+        }
+
+        internal void reactArea()   
+        {
+            area = param1 * param2;
+        }
+    }
+}

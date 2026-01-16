1f74e3a351075ae834d3323e6692e36c4dbafdbb
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-01-16 09:00:27 +0530

    Sealed Class Cant inherits

---
+++ ShivaGautamCsharpCourseNotes/9_1AccessModifiers/SealedClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShivaGautamCsharpCourseNotes._9_1AccessModifiers
+{
+    sealed class SealedClass
+    {
+        // Sealed Class :- if we want to protect class from inheritance or avoid inheritantce 
+                         // - we cant inherites sealed class. we cant access in main class we only acces it same class.
+
+        internal void func()
+        {
+            Console.WriteLine("Sealed Class");
+        }
+
+    }
+}
--- ShivaGautamCsharpCourseNotes/obj/Debug/net8.0/ShivaGautamCsharpCourseNotes.AssemblyInfo.cs
+++ ShivaGautamCsharpCourseNotes/obj/Debug/net8.0/ShivaGautamCsharpCourseNotes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShivaGautamCsharpCourseNotes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1255e08994feef02d32c8d98602904521023abc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a748cab7da0794859b400c91f2eadae6418f254")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShivaGautamCsharpCourseNotes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShivaGautamCsharpCourseNotes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

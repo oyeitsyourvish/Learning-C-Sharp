3239f789067571f7eeaa343ff444a384483969d0
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-01-19 13:03:38 +0530

    Example of Multiple catch Block use

---
+++ ShivaGautamCsharpCourseNotes/17ExceptionHandling/MultiCatchBlock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace ShivaGautamCsharpCourseNotes._17ExceptionHandling
+{
+    internal class MultiCatchBlock
+    {
+        public void Run()
+        {
+            try
+            {
+                Console.WriteLine("Enter the first number:");
+                int a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter the second number:");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                int c = a / b;
+                Console.WriteLine("Result: " + c);
+            }
+            catch (FormatException) // specific exception for invalid format
+            {
+                Console.WriteLine("Input was not a valid integer.");
+            }
+            catch (DivideByZeroException) // specific exception for division by zero
+            {
+                Console.WriteLine("Cannot divide by zero.");
+            }
+            catch (OverflowException) // only for int32
+            {
+                Console.WriteLine("The number entered is too large or too small.");
+            }
+            catch (Exception ex)  // overall exception handler
+            {
+                Console.WriteLine("Unexpected error: " + ex.Message);
+            }
+        }
+    }
+}

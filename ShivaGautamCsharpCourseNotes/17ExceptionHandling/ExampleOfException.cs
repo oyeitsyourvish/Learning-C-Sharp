bb5e953f3affd9d1b2e27890b0840a003b3c5385
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-01-19 12:41:27 +0530

    Explaination of Exception Handling

---
+++ ShivaGautamCsharpCourseNotes/17ExceptionHandling/ExampleOfException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShivaGautamCsharpCourseNotes._17ExceptionHandling
+{
+    internal class ExampleOfException
+    {
+        internal void Exception()
+        {
+            int a = 10, b = 0;
+
+            // here we got this exception: System.DivideByZeroException
+            int c = a / b;  // it goes to infinite loop so we need to handle exception.
+            Console.WriteLine(c);
+
+        }
+    }
+}
---
+++ ShivaGautamCsharpCourseNotes/17ExceptionHandling/ExeptionHandMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShivaGautamCsharpCourseNotes._17ExceptionHandling
+{
+    internal class ExeptionHandMain
+    {
+        /* Exception Handling :- it use to handle unexcpted errror
+          - it use to smoothly terminate the program in case of runtime error.
+         
+         */
+        public static void Main(string[] args)
+        {
+            ExampleOfException e = new ExampleOfException();
+            e.Exception();
+        }
+
+    }
+}

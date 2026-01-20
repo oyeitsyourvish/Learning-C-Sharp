aca1fac979d7d0a21c43b86bf4577cbd74b3d900
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-01-20 23:46:21 +0530

    Explanation of File Handling

---
+++ ShivaGautamCsharpCourseNotes/18FileHandling/FileHandlingMain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShivaGautamCsharpCourseNotes._18FileHandling
+{
+    internal class FileHandlingMain
+    {
+        /* FILE HANDLING in C# :- if we want to store program output permanently under computer hard disk
+         then we use file handling Operation.
+         - C# provide System.IO namespace to perform file handling operation.
+            1.Create a file
+            2.Write data into file
+            3.Read data from file
+            4.Append a file (to merge data into existing data)
+            5.Delete a file
+         */
+    }
+}
--- ShivaGautamCsharpCourseNotes/obj/Debug/net8.0/ShivaGautamCsharpCourseNotes.AssemblyInfo.cs
+++ ShivaGautamCsharpCourseNotes/obj/Debug/net8.0/ShivaGautamCsharpCourseNotes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShivaGautamCsharpCourseNotes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1abc9e9eecf8eee124ae7e08744b956063c07710")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f28e7a3222f346bc1f6b8cf79cb4d98f83e88a3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShivaGautamCsharpCourseNotes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShivaGautamCsharpCourseNotes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

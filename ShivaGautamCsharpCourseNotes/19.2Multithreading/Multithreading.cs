8d671b6fcc1872752f2cadd18e1c35770b5aeb93
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-01-27 00:25:18 +0530

    Exaplanation of Thread, Process, Single And Multiple Thread

--- ShivaGautamCsharpCourseNotes/19.2Multithreading/Multithreading.cs
+++ ShivaGautamCsharpCourseNotes/19.2Multithreading/Multithreading.cs
@@ -24,7 +24,18 @@ namespace CSharpBasicToAdvanced._19._2Multithreading
                 - In a web browser, multiple tabs can load content simultaneously.
                 - In a word processor, spell checking can run in the background while you type.
                 - In a game, rendering graphics and processing user input can happen concurrently.
-                - While downloading a file, you can still scroll UI Server can handle many client requests at once  
+                - While downloading a file, you can still scroll UI Server can handle many client requests
+                  at once  
+
+            @ Process vs Thread
+                  % Process :- Running program (EXE) Has its own memory space.
+                  % Thread  :- Smallest unit of execution inside a process Shares memory with
+                               other threads of same process
+            NOTE :-  One process can have many threads.
+
+            @ Single Threading vs Multithreading :-
+                  % Single Threading :- One thread executes one task at a time
+                  % Multithreading   :- Multiple threads execute multiple tasks simultaneously
              */
         }
     }

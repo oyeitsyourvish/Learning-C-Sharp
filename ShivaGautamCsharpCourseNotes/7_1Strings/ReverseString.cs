707a2841fe675add8b13ed7917189b40eee2c8fb
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-06-20 07:30:52 +0530

    Added reverse String Program

---
+++ ShivaGautamCsharpCourseNotes/7_1Strings/ReverseString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasicToAdvanced._7_1Strings
+{
+    internal class ReverseString
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter the string");
+            string a = Console.ReadLine();
+
+            string reversed = " ";
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                reversed += a[i];
+            }
+
+            Console.WriteLine(reversed);
+        }
+    }
+}

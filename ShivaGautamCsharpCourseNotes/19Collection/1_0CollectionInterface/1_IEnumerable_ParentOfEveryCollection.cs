07014831bdf0710f401074d75d9a358be23c670a
oyeitsyourvish <chinchanevishal03@gmail.com>
2026-07-02 12:11:22 +0530

    Explaination of Collection Interface Or Parent of almost all Collection IEnumerable and its Child ICollection

---
+++ ShivaGautamCsharpCourseNotes/19Collection/1_0CollectionInterface/1_IEnumerable_ParentOfEveryCollection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CSharpBasicToAdvanced._19Collection._1_0CollectionInterface
+{
+    internal class IEnumerable_ParentOfEveryCollection
+    {
+        public static void Main(string[] args)
+        {
+            // IEnumerable : it is Parent of almost all collection.
+            /*   Purpose :- Only allows reading data one by one.
+                 Methods :- GetEnumerator()
+            */
+
+            List<int> num = new List<int>
+            { 10,
+              20,
+              40,
+              30
+            };
+
+            foreach (int i in num)
+            {
+                Console.WriteLine(i);
+            }
+            // foreach internally uses IEnumerable and its GetEnumerator (reading data one by one
+            // (real life ex. we read a book and using finger on every word which we read or point that word))
+
+        }
+    }
+}
---
+++ ShivaGautamCsharpCourseNotes/19Collection/1_0CollectionInterface/2_ICollection_AfterIEnumerable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasicToAdvanced._19Collection._1_0CollectionInterface
+{
+    internal class _2_ICollection_AfterIEnumerable
+    {
+        public static void Main(string[] args)
+        {
+            // ICollection : it is child of IEnumerable. and add collection features.
+            /*   Purpose :- It allows reading and writing data one by one.
+                 Methods :- Count, Add, Remove, Clear, Contains, CopyTo
+            */
+            ICollection<int> num = new List<int>();
+
+            // Add elements to the collection using the Add method.
+            num.Add(10);
+            num.Add(30);
+            num.Add(20);
+            num.Add(10);
+
+            foreach (int i in num)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Count the number of elements in the collection using the Count property.
+            Console.WriteLine("Number of elements in the collection: " + num.Count);
+
+            // Remove an element from the collection using the Remove method.
+            num.Remove(10);
+            foreach (int i in num)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Removed 10 from the collection.");
+
+            // Check if an element exists in the collection using the Contains method.
+            if (num.Contains(20))
+            {
+                Console.WriteLine("20 exists in the collection.");
+            }
+
+            // Clear all elements from the collection using the Clear method.
+            num.Clear();
+            //it cant be used in foreach loop because it is empty now.
+            foreach (var i in num)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Cleared all elements from the collection.");
+
+        }
+    }
+}
